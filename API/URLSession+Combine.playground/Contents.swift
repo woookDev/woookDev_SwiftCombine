8c3d304d6ae6e8eeec569a95cfcbcad01f27e7f8
woookdev <hjw0911@gmail.com>
2021-03-16 22:50:19 +0900

    Add dataTaskPublisher

---
+++ API/URLSession+Combine.playground/Contents.swift
@@ -0,0 +1,26 @@
+import UIKit
+
+var str = "Hello, playground"
+
+func test() {
+  
+}
+
+let url = URL(string: "https://mysite.com/mydata.json")!
+
+// 1
+let subscription = URLSession.shared
+  // 2
+  .dataTaskPublisher(for: url)
+  .sink { completion in
+    // 3
+    if case .failure(let err) = completion {
+      print("Retrieving data failed with error \(err)")
+    }
+  } receiveValue: { data, response in
+    // 4
+    print("Retrieved data of size \(data.count), response = \(response)")
+  }
+
+test()
+
