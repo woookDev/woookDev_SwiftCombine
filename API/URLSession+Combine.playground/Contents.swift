14f5c1be0caf22b4a6f77e4bc8e82af7eb813947
woookdev <hjw0911@gmail.com>
2021-03-16 23:33:05 +0900

    Add multicast operator test

--- API/URLSession+Combine.playground/Contents.swift
+++ API/URLSession+Combine.playground/Contents.swift
@@ -26,5 +26,35 @@ let subscription = URLSession.shared
     print("Retrieved data of size \(data.count), response = \(response)")
   }
 
+let url2 = URL(string: "https://www.raywenderlich.com")!
+let publisher = URLSession.shared
+// 1
+  .dataTaskPublisher(for: url2)
+  .map(\.data)
+  .multicast { PassthroughSubject<Data, URLError>() }
+
+// 2
+let subscription1 = publisher
+  .sink(receiveCompletion: { completion in
+          if case .failure(let err) = completion {
+            print("Sink1 Retrieving data failed with error \(err)")
+          }
+  }, receiveValue: { object in
+    print("Sink1 Retrieved object \(object)")
+  })
+
+// 3
+let subscription2 = publisher
+  .sink(receiveCompletion: { completion in
+    if case .failure(let err) = completion {
+      print("Sink2 Retrieving data failed with error \(err)")
+    }
+  }, receiveValue: { object in
+    print("Sink2 Retrieved object \(object)")
+  })
+
+// 4
+let subscription = publisher.connect()
+
 test()
 
