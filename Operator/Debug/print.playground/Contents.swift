1732da5cd4e5263425136737632e9643f9f2e9a1
woookdev <hjw0911@gmail.com>
2021-03-19 20:11:31 +0900

    Add TextOutputStream for print

--- Operator/Debug/print.playground/Contents.swift
+++ Operator/Debug/print.playground/Contents.swift
@@ -1,6 +1,25 @@
 import UIKit
 import Combine
 
+class TimeLogger: TextOutputStream {
+  private var previous = Date()
+  private let formatter = NumberFormatter()
+  
+  init() {
+    formatter.maximumFractionDigits = 5
+    formatter.minimumFractionDigits = 5
+  }
+  
+  func write(_ string: String) {
+    let trimmed = string.trimmingCharacters(in: .whitespacesAndNewlines)
+    guard !trimmed.isEmpty else { return }
+    let now = Date()
+    print("+\(formatter.string(for: now.timeIntervalSince(previous))!)s: \(string)")
+    previous = now
+  }
+}
+
+
 let subscription = (1...3).publisher
-  .print()
+  .print("publisher", to: TimeLogger())
   .sink { _ in }
