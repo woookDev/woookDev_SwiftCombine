65300235eff2b3b831ad72d81077e05effce91da
woookdev <hjw0911@gmail.com>
2021-03-20 20:54:52 +0900

    Add PureSwift not available in KVO

--- KVO+Combine.playground/Contents.swift
+++ KVO+Combine.playground/Contents.swift
@@ -8,11 +8,18 @@ let subscription = queue
     print("Outstanding operations in queue: \($0)")
   }
 
+struct PureSwift {
+  let a: (Int, Bool)
+}
 
 // 1
 class TestObject: NSObject {
   // 2
   @objc dynamic var integerProperty: Int = 0
+  @objc dynamic var stringProperty: String = ""
+  @objc dynamic var arrayProperty: [Float] = []
+  
+  // @objc dynamic var structProperty: PureSwift = .init(a: (0, false))
 }
 
 let obj = TestObject()
@@ -20,11 +27,27 @@ let obj = TestObject()
 // 3
 let subscription2 = obj.publisher(for: \.integerProperty)
   .sink {
-  print("integerProperty changes to \($0)_")
-}
+    print("integerProperty changes to \($0)")
+  }
+
+let subscription3 = obj.publisher(for: \.stringProperty)
+  .sink {
+    print("stringProperty changes to \($0)")
+  }
+
+let subscription4 = obj.publisher(for: \.arrayProperty)
+  .sink {
+    print("arrayProperty changes to \($0)")
+  }
+
 
 // 4
 obj.integerProperty = 100
 obj.integerProperty = 200
+
+obj.stringProperty = "Hello"
+obj.arrayProperty = [1.0]
+obj.stringProperty = "World"
+obj.arrayProperty = [1.0, 2.0]
 
 
